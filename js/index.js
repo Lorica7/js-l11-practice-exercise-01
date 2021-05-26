a575cf912aecd34b95f9cbd6ea03b6ae50a4e2c0
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-05-26 17:41:39 -0400

    added  menu selection

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const randomFolks = document.querySelector(".random-peeps");
+const selectUserNumber = document.querySelector("#users");
 
-
-const getData = async () => {
-    const usersReq = await fetch("https://randomuser.me/api?results=5")
+const getData = async (numUsers) => {
+    const usersReq = await fetch(`https://randomuser.me/api?results=${numUsers}`)
     const data = await usersReq.json();
     const userResults = data.results
    displayUsers(userResults)
@@ -18,12 +18,17 @@ const displayUsers = function (userResults) {
         const imageUrl = user.picture.medium;
         const userDiv = document.createElement("div")
         userDiv.innerHTML = `
-        <h3>${name}</h3>
+        <h3>${name.first} ${name.last}</h3>
         <p>${country}</p>
         <img src=${imageUrl} alt="User avatar" />
     `;
         randomFolks.append(userDiv)
     }
-
 }
-getData()
+
+
+selectUserNumber.addEventListener("change", function (event) {
+    const numUsers = event.target.value
+    getData(numUsers)
+    
+ })
