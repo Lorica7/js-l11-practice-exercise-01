7010bdf4b304578f31d968221a939aca037ee14e
Lorica7 <38511443+Lorica7@users.noreply.github.com>
2021-05-26 13:20:23 -0400

    practice exercise finished

--- js/index.js
+++ js/index.js
@@ -1,1 +1,29 @@
 const randomFolks = document.querySelector(".random-peeps");
+
+
+const getData = async () => {
+    const usersReq = await fetch("https://randomuser.me/api?results=5")
+    const data = await usersReq.json();
+    const userResults = data.results
+   displayUsers(userResults)
+    
+}
+
+
+const displayUsers = function (userResults) {
+    randomFolks.innerHTML = "";
+    for (let user of userResults) {
+        const country = user.location.country;
+        const name = user.name;
+        const imageUrl = user.picture.medium;
+        const userDiv = document.createElement("div")
+        userDiv.innerHTML = `
+        <h3>${name}</h3>
+        <p>${country}</p>
+        <img src=${imageUrl} alt="User avatar" />
+    `;
+        randomFolks.append(userDiv)
+    }
+
+}
+getData()
